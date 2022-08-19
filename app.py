395980270ad978c9ac795a71d019c7865e299f87
davejang <davejang98@naver.com>
2022-08-19 22:54:09 +0900

    requirements

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from inference import result
+
+def revise(sentence):
+    words = sentence.split()
+    result = []
+    for word in words:
+        tmp = ''    
+        for t in word:
+            if not tmp:
+                tmp += t
+            elif tmp[-1]!= t:
+                tmp += t
+        result.append(tmp)
+    return ' '.join(result)
 
 app = Flask(__name__)
 
-data = result()
-data = data[0]
-
 @app.route('/')
 def hello():
+    file = ''
+    data = result(file)
+    data = revise(data[0])
+    return data    
+
+@app.route('/inference',methods=['POST'])
+def inference():
+    file = request.files.get('file');
+    data = result(file)
+    data = revise(data[0])
     return data
 
 if __name__ == '__main__':
